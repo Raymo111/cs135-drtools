a96ddadd1a3a11e8240bae4d5669ab58f4f73ca3
SantaKaus <44451907+SantaKaus@users.noreply.github.com>
2020-10-10 22:56:26 -0400

    Add files via upload

---
+++ scripts/autocomplete.rkt
@@ -0,0 +1,123 @@
+;;
+;; **************************************************************
+;;    Copyright (c) Raymond Li <racket@raymond.li> 2020
+;;    Code completion for DrRacket
+;;    Last updated 2020-09-22
+;;    Modified from complete-word from quickscript extras repo
+;; **************************************************************
+;;
+
+#lang racket/base
+(require racket/class
+         racket/dict
+         racket/list
+         quickscript)
+
+(script-help-string "Autocomplete by https://Raymond.Li")
+;; Replaces the text abbreviation right before the caret with stuff
+
+(define words
+  '(
+    ; custom:
+    ("cx"  "(check-expect (" ") )")
+    ("cw"  "(check-within (" ")  tolerance)")
+    ("ce"  "(check-error (" ") )")
+    ("df"  "(define (" ")\n  ())")
+    ("d"   "(define " ")")
+    ("t"   "true" "")
+    ("f"   "false" "")
+
+    ; custom design recipe:
+    ("hdr" ";;
+;; ***************************************************
+;;    Raymond Li (studentID)
+;;    CS 135 Fall 2020
+;;    Assignment " ", Problem
+;; ***************************************************
+;;\n")
+    ("cmt" ";;\n;; "       "\n;;")
+    ("l;"  ";;   "         "")
+    ("ppe" ";; ("          ") \n;; Examples:\n")
+    ("ctr" ";; "           ":  -> \n")
+    ("req" ";; Requires: " "\n")
+    ("tst" ";; Tests:\n"   "")
+    ("ctd"  ";; (countdown n) " "
+;; Examples:
+(check-expect (countdown 0) (cons 0 empty) )
+(check-expect (countdown 2) (cons 2 (cons 1 (cons 0 empty))))\n\n
+;; countdown: Nat -> (listof Nat)
+(define (countdown n) ...
+  (cond [(zero? n) (cons 0 empty)]
+        [else (cons n (countdown (sub1 n)))]))\n
+;; Tests")
+    ("nelox"  ";; (ne-listof-X-template nelox) " "
+;; Examples:
+(check-expect (ne-listof-X-template (cons X empty)) Answer)
+(check-expect (ne-listof-X-template (cons X (cons Y empty))) Answer)\n\n
+;; ne-listof-X-template: (ne-listof X) -> Any
+(define (ne-listof-X-template nelox) ...
+  (cond [(empty? (rest nelox) (...(first nelox))]
+        [else (... (first nelox)
+                   (rest nelox))])\n
+;; Tests")
+    ("lox"  ";; (listof-X-template lox) " "
+;; Examples:
+(check-expect (listof-X-template empty) )
+(check-expect (listof-X-template (cons  empty)) )\n\n
+;; listof-X-template: (listof ) ->
+(define (listof-X-template lox) ...
+  (cond [(empty? lox) ...]
+        [(cons? lox) ... (first lox)
+                     ... (listof-X-template (rest lox)) ...]))\n
+;; Tests")
+
+    #| Defaults from complete-word plugin of quickscript extras
+    ("dsr"   "(define-syntax-rule (" ")\n  )")
+    ("ds"    "(define-syntax " "\n  )")
+    ("sr"    "(syntax-rules ()\n    [(_ " ")])")
+    ("sc"    "(syntax-case stx ()\n    [(_ " ")])")
+    ("dsm"   "(define-simple-macro (" ")\n  )")
+    ("lbd"   "(λ (" ") ")
+    ("param" "(parameterize ([current-" "])\n  )")
+    ("wh"    "(with-handlers ([exn:" "])\n  )")
+    ("wiff"  "(with-input-from-file " "\n  (λ _ ))")
+    ("wotf"  "(with-output-to-file " " #:exists 'replace\n  (λ _ ))")
+
+    ; slideshow:
+    ("slide"    "(slide #:title \"" "\"\n       )")
+    ("item"     "@item{" "}")
+    ("subitem"  "@subitem{" "}")
+    ("$"        "@${" "}")
+    ("$$"       "@$${" "}")
+    |#))
+
+(define-script autocomplete
+  #:label "Autocomplete"
+  #:shortcut #\ 
+  #:shortcut-prefix (ctl)
+  (λ (s #:editor ed)
+    (define pos (send ed get-end-position))
+    (define str
+      (send ed get-text
+            (send ed get-backward-sexp pos)
+            pos))
+    (define str-ext (dict-ref words str #f))
+    (define left (if (list? str-ext) (first str-ext) str-ext))
+    (define right (and (list? str-ext) (second str-ext)))
+    (when str-ext
+      (send ed begin-edit-sequence)
+      (send ed select-backward-sexp)
+      (send ed insert left)
+      (when right
+        (define ipos (send ed get-start-position))
+        (send ed insert right)
+        (send ed set-position ipos))
+      (send ed end-edit-sequence))
+    #f))
+
+#;(
+   item
+   para
+   wh
+   $$
+   )
