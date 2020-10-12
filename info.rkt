d5bfc51f6cfc9da8db46d1f8a3098c234ffb7c7b
SantaKaus <44451907+SantaKaus@users.noreply.github.com>
2020-10-12 13:55:16 -0400

    Update info.rkt

--- info.rkt
+++ info.rkt
@@ -7,7 +7,7 @@
 (define collection "cs135-drtools")
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/drcomplete-uwaterloo.scrbl" ())))
+(define scribblings '(("scribblings/cs135-drtools.scrbl" ())))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(kaus0))
